696bac993be6c1d28f043219f7ebd3302970b806
Mandi Haase <haase1020@gmail.com>
2020-03-24 14:05:40 -0400

    follow instructor halfway

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); //install this npm package
 
 const Users = require('../users/users-model.js');
 
@@ -25,16 +26,33 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token,  //send the token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
     })
-    .catch(error => {
-      res.status(500).json(error);
+    .catch(({name, code, message, stack}) => {
+      res.status(500).json({name, code, message, stack});
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+
+  const secret = process.env.JWT_SECRET || "is this secret, is it safe?";
+
+  const options = {
+    expiresIn: '1h',
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
